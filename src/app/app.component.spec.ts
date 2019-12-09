264142e7ecc3a95409bbd5a88012eddf555f357f
Rusley Santos <rusley.santos@spassu.com.br>
2019-12-09 16:22:08 -0300

    Altera versao angular

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,10 +26,10 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('CadastroPessoa');
   });
 
-  it('should render title in a h1 tag', () => {
+  it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to CadastroPessoa!');
+    expect(compiled.querySelector('.content span').textContent).toContain('CadastroPessoa app is running!');
   });
 });
